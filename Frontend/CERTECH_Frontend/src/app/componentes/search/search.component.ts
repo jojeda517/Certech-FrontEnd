2c7219e4eeecc592dbf7d1510f45dd1bf37b389f
Anthony S <asolis7378@uta.edu.ec>
2023-12-06 23:45:44 -0500

    Creacion de Componente para modulos de la pagina principal

--- Frontend/CERTECH_Frontend/src/app/componentes/search/search.component.ts
+++ Frontend/CERTECH_Frontend/src/app/componentes/search/search.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header.component";
+import { ModulosComponent } from "../modulos/modulos.component";
 
 @Component({
     selector: 'app-search',
     standalone: true,
     templateUrl: './search.component.html',
     styleUrl: './search.component.css',
-    imports: []
+    imports: [ModulosComponent]
 })
 export class SearchComponent {
 
