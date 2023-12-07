c9bb31b99d03ee26edb7bffd27e0906bccc5f269
Anthony S <asolis7378@uta.edu.ec>
2023-12-07 09:19:36 -0500

    Creacion de cards para cursos

--- Frontend/CERTECH_Frontend/src/app/componentes/search/search.component.ts
+++ Frontend/CERTECH_Frontend/src/app/componentes/search/search.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header.component";
 import { ModulosComponent } from "../modulos/modulos.component";
+import { CardsComponent } from "../cards/cards.component";
 
 @Component({
     selector: 'app-search',
     standalone: true,
     templateUrl: './search.component.html',
     styleUrl: './search.component.css',
-    imports: [ModulosComponent]
+    imports: [ModulosComponent, CardsComponent]
 })
 export class SearchComponent {
 
