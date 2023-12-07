c16f1e742ff71aa006a966d0b6429aecdc493a72
Anthony S <asolis7378@uta.edu.ec>
2023-12-06 23:19:06 -0500

    Error Correguido buscador pagina principal

--- Frontend/CERTECH_Frontend/src/app/app.routes.ts
+++ Frontend/CERTECH_Frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
-
 import {  Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './componentes/search/search.component';
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: '', component: SearchComponent },
 
 ];
