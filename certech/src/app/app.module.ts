7ac16607def03eda15dfba9898dbab99e00bdebd
Jonathan Villafuerte <jvillafuerte9952@uta.edu.ec>
2024-01-03 14:40:49 -0500

    Arreglo de estructua oara models

--- certech/src/app/app.module.ts
+++ certech/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { HeaderComponent } from './estructura/header/header.component';
+import { CardsComponent } from './estructura/componentes/cards/cards.component';
+import { ModulosComponent } from './estructura/componentes/modulos/modulos.component';
+import { SearchComponent } from './estructura/componentes/search/search.component';
+import { NavbarComponent } from './estructura/componentes/navbar/navbar.component';
+import { FirmasComponent } from './paginas/firmas/firmas.component';
+import { LoginComponent } from './paginas/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CardsComponent,
+    ModulosComponent,
+    SearchComponent,
+    NavbarComponent,
+    FirmasComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
