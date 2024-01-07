b7b24d1f16114013f65f9cd80f2d20f868ed8faa
Jonathan Villafuerte <jvillafuerte9952@uta.edu.ec>
2024-01-07 18:02:48 -0500

    search certificados

--- certech/src/app/estructura/componentes/search/search.component.ts
+++ certech/src/app/estructura/componentes/search/search.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
@@ -6,12 +7,42 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent  {
+  cedula: string = "";
+  certificados: any[] = [];
 
-  @Output() searchEvent = new EventEmitter<string>();
-  searchCedula: string = '';
+  constructor(private http: HttpClient) { }
 
-  search(): void {
-    // Emitir el evento de búsqueda con el número de cédula
-    this.searchEvent.emit(this.searchCedula);
+  buscarCertificados() {
+    const apiUrl = `http://34.125.254.116:8000/api/CertificadoParticipante/${this.cedula}/`;
+
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.certificados = data.certificados;
+      this.obtenerNombresEventos();
+    });
+  }
+
+  obtenerNombresEventos() {
+    // Obtener los nombres de los eventos de manera asíncrona
+    const eventosPromises = this.certificados.map(certificado =>
+      this.http.get(`http://34.125.254.116:8000/api/evento/${certificado.id_evento}/`)
+        .toPromise()
+    );
+
+    // Manejar las promesas usando Promise.all
+    Promise.all(eventosPromises)
+      .then((eventosData: any[]) => {
+        eventosData.forEach((data, index) => {
+          // Asignar el nombre del evento al certificado correspondiente
+          this.certificados[index].nombre_evento = data.nombre_evento;
+        });
+      })
+      .catch(error => {
+        console.error('Error al obtener nombres de eventos:', error);
+      });
+  }
+
+  getNombreEvento(idEvento: number): string {
+    const certificado = this.certificados.find(cert => cert.id_evento === idEvento);
+    return certificado ? certificado.nombre_evento : 'Nombre no encontrado';
   }
 }
--- certech/src/app/servicios/certificados.service.ts
+++ certech/src/app/servicios/certificados.service.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CertificadosService {
+  private apiUrl = 'http://34.125.254.116:8000/api/certificados/';
+
   constructor(private http: HttpClient) {}
 
-  getDatosParaCertificado(): Observable<any> {
-    // Aquí deberías hacer una llamada HTTP a tu servidor para obtener los datos necesarios
-    // por ejemplo, la firma, el nombre de la autoridad, etc.
-    return this.http.get<any>('URL_DE_TU_API_PARA_OBTENER_DATOS');
+  // Otros métodos del servicio...
+
+  buscarCertificados(filtro: string): Observable<any> {
+    const url = `${this.apiUrl}?filtro=${filtro}`;
+    return this.http.get<any>(url);
   }
 }
